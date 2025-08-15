73919a7c11093b3194e774a38579f22372c855b5
ugorphpgo <ugorphpgo@gmail.com>
2025-08-16 00:59:35 +0300

    Update files.go

--- files/files.go
+++ files/files.go
@@ -4,6 +4,7 @@ import (
 	"demo/password/files"
 	"encoding/json"
 	"github.com/fatih/color"
+	"strings"
 	"time"
 )
 
@@ -26,17 +27,55 @@ func NewVault() *Vault {
 		color.Red(err.Error())
 	}
 	return &vault
+
 }
 
 func (vault *Vault) AddAccount(acc Account) {
 	vault.Accounts = append(vault.Accounts, acc)
-	vault.UpdateAt = time.Now()
+	vault.save()
 }
 
+func (vault *Vault) FindAccountsByUrl(url string) []Account {
+	var accounts []Account
+	for _, account := range vault.Accounts {
+		isMatched := strings.Contains(account.Url, url)
+		if isMatched {
+			accounts = append(accounts, account)
+		}
+
+	}
+	return accounts
+}
+
+func (vault *Vault) DeleteAccountsByUrl(url string) bool {
+	var accounts []Account
+	isDeleted := false
+	for _, account := range vault.Accounts {
+		isMatched := strings.Contains(account.Url, url)
+		if !isMatched {
+			accounts = append(accounts, account)
+			isDeleted = true
+		}
+
+	}
+	vault.Accounts = accounts
+	vault.save()
+
+	return isDeleted
+}
 func (Vault *Vault) ToBytes() ([]byte, error) {
 	file, err := json.Marshal(Vault)
 	if err != nil {
 		return nil, err
 	}
 	return file, nil
+}
+
+func (vault *Vault) save() {
+	vault.UpdateAt = time.Now()
+	data, err := vault.ToBytes()
+	if err != nil {
+		color.Red("Не удалось преобразовать данные  в json")
+	}
+	files.WriteFile(data, "data.json")
 }
