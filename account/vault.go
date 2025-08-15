93d81dff5bc9a76f84adaada50f94eb35226006c
ugorphpgo <ugorphpgo@gmail.com>
2025-08-16 00:33:19 +0300

    added DeleteAccountsByUrl Method

--- account/vault.go
+++ account/vault.go
@@ -1,7 +1,10 @@
 package account
 
 import (
+	"demo/password/files"
 	"encoding/json"
+	"github.com/fatih/color"
+	"strings"
 	"time"
 )
 
@@ -11,17 +14,65 @@ type Vault struct {
 }
 
 func NewVault() *Vault {
-	return &Vault{
-		Accounts: []Account{},
-		UpdateAt: time.Now(),
+	file, err := files.ReadFile("data.json")
+	if err != nil {
+		return &Vault{
+			Accounts: []Account{},
+			UpdateAt: time.Now(),
+		}
 	}
+	var vault Vault
+	err = json.Unmarshal(file, &vault)
+	if err != nil {
+		color.Red(err.Error())
+	}
+	return &vault
+
 }
 
 func (vault *Vault) AddAccount(acc Account) {
 	vault.Accounts = append(vault.Accounts, acc)
 	vault.UpdateAt = time.Now()
+	data, err := vault.ToBytes()
+	if err != nil {
+		color.Red("Не удалось преобразовать данные  в json")
+	}
+	files.WriteFile(data, "data.json")
 }
 
+func (vault *Vault) FindAccountsByUrl(url string) []Account {
+	var accounts []Account
+	for _, account := range vault.Accounts {
+		isMatched := strings.Contains(account.Url, url)
+		if isMatched {
+			accounts = append(accounts, account)
+		}
+
+	}
+	return accounts
+}
+
+func (vault *Vault) DeleteAccountsByUrl(url string) bool {
+	var accounts []Account
+	isDeleted := false
+	for _, account := range vault.Accounts {
+		isMatched := strings.Contains(account.Url, url)
+		if !isMatched {
+			accounts = append(accounts, account)
+			isDeleted = true
+		}
+
+	}
+	vault.Accounts = accounts
+	vault.UpdateAt = time.Now()
+	data, err := vault.ToBytes()
+	if err != nil {
+		color.Red("Не удалось преобразовать данные  в json")
+	}
+	files.WriteFile(data, "data.json")
+
+	return isDeleted
+}
 func (Vault *Vault) ToBytes() ([]byte, error) {
 	file, err := json.Marshal(Vault)
 	if err != nil {
