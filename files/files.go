b4af5626126fd634b13a5a3384d88e501ade0dfd
ugorphpgo <ugorphpgo@gmail.com>
2025-08-15 21:43:32 +0300

    Update files.go

--- files/files.go
+++ files/files.go
@@ -1,30 +1,42 @@
-package files
+package account
 
 import (
-	"fmt"
-	"os"
+	"demo/password/files"
+	"encoding/json"
+	"github.com/fatih/color"
+	"time"
 )
 
-func ReadFile() {
-	data, err := os.ReadFile("data.json")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(data))
+type Vault struct {
+	Accounts []Account `json:"accounts"`
+	UpdateAt time.Time `json:"updateAt"`
 }
 
-func WriteFile(content []byte, name string) {
+func NewVault() *Vault {
+	file, err := files.ReadFile("data.json")
+	if err != nil {
+		return &Vault{
+			Accounts: []Account{},
+			UpdateAt: time.Now(),
+		}
+	}
+	var vault Vault
+	err = json.Unmarshal(file, &vault)
+	if err != nil {
+		color.Red(err.Error())
+	}
+	return &vault
+}
 
-	file, err := os.Create(name)
+func (vault *Vault) AddAccount(acc Account) {
+	vault.Accounts = append(vault.Accounts, acc)
+	vault.UpdateAt = time.Now()
+}
+
+func (Vault *Vault) ToBytes() ([]byte, error) {
+	file, err := json.Marshal(Vault)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-	_, err = file.Write(content)
-	defer file.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Успешная запись")
+	return file, nil
 }
